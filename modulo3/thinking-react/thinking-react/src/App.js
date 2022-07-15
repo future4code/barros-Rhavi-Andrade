889c7b97f172d2bdb4d00f4e472fcb34f79edabf
Rhavi Marques Cordeiro Andrade <rhavimarques@gmail.com>
2022-07-14 22:04:35 -0300

    parquinho ta pegando fogo

---
+++ modulo3/thinking-react/thinking-react/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+//THIS WORKS
+function App() {
+  return (
+    <div className="App">
+      <header />
+      <body>
+        <filtros />
+        <produtos />
+
+
+        <carrinho />
+      </body>
+      <prefooter />
+      <footer />
+    </div>
+  );
+}
+
+
+
+export default App;
+
+
